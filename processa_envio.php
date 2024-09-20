6c1b3c1d86ee39e803716dea058e1c117f386d97
unknown <xtsunammyx@gmail.com>
2024-09-20 20:31:06 -0300

    adicionando comentarios

--- processa_envio.php
+++ processa_envio.php
@@ -56,14 +56,14 @@ try {
     $mail->isSMTP();                                            //Send using SMTP
     $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail->Username   = 'xtsunammyx@gmail.com';                     //SMTP username
+    $mail->Username   = 'exemplo@gmail.com';                     //SMTP username
     $mail->Password   = 'lkxe ajuv xtdw fqpq';                               //SMTP password
     $mail->SMTPSecure = 'tls';
     //$mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         //Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
     $mail->Port       = 587;                                    //TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
     //Recipients
-    $mail->setFrom('xtsunammyx@gmail.com', 'Remetente');
+    $mail->setFrom('exemplo@gmail.com', 'Remetente');// email de quem vai receber
     $mail->addAddress($mensagem->__get("para"));     //Add a recipient
     //$mail->addAddress('ellen@example.com');               //Name is optional
     //$mail->addReplyTo('info@example.com', 'Information'); //contato padrão
